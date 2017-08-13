e8bfcd1190f686f30d17581693a131957ca7ba7c
DESKTOP-PE8APUQ\ScorpionIPX <scorpionipx@gmail.com>
2017-08-13 21:57:42 +0300

    Created base project files

---
+++ RemoteCar/RemoteCar/main.c
@@ -0,0 +1,18 @@
+/*
+ * RemoteCar.c
+ *
+ * Created: 13-Aug-17 9:54:53 PM
+ * Author : ScorpionIPX
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
